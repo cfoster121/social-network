d5e4683d4b3cf3b977796ffc3dc25d3452d65970
Courtney Foster <cfoster649@gmail.com>
2022-02-23 15:39:32 -0800

    Created thought model

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 // Require schema and model from mongoose
 const mongoose = require('mongoose');
 
+//USER//
 // Construct a new instance of the schema class
 const userSchema = new mongoose.Schema({
     username: {
@@ -24,7 +25,31 @@ const userSchema = new mongoose.Schema({
     friends: [userSchema]
 });
 
-//Create user model via userSchema
+//Create User model via userSchema
 const User = model('user', userSchema);
 
-module.exports = User;
+//THOUGHT//
+// Construct a new instance of the schema class
+const thoughtSchema = new mongoose.Schema({
+    thoughtText: {
+        type: String,
+        required: [true, 'Please enter a thought'],
+        minLength: 1,
+        maxLength: 280
+    },
+    createdAt: {
+        date: Date
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: [true, 'Please enter a username'],
+    },
+    reactions: [reactionSchema]
+})
+
+//Create Thought model via userSchema
+const Thought = model('thought', thoughtSchema);
+
+
+module.exports = { User, Thought, Reaction }
