12f65a1694e80b9a89c73c9acaa8585d930b4fc2
Courtney Foster <cfoster649@gmail.com>
2022-02-23 16:55:35 -0800

    Set up user routes js page

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:users/:userId').get(getSingleUser).post(updateUser).delete(deleteUser);
+
+// /api/students/:studentId/assignments
+router.route('/:studentId/assignments').post(addAssignment);
+
+// /api/users/:userId/friends/:friendId
+router.route('/api/users/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+
+module.exports = router;
